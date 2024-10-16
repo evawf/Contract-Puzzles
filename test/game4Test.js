69e532d7a648bfec30b99061325d77230671c9a7
eva.wang <eva.wang81@gmail.com>
2024-10-16 22:20:10 +0800

    done 3 tests

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,14 +1,14 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
@@ -16,6 +16,6 @@ describe('Game4', function () {
     await game.win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
